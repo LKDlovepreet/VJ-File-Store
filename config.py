0745ee9401f3fe8c41ecefc93216e45bc664cdfc
LKDlovepreet <singhlovepreet05198@gmail.com>
2025-01-14 13:10:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6261595158').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "@videosstoragebot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "videosstoragebot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
